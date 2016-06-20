a9e9aed850db824ce00678e6252e273371502b60
Romain Ruaud <romain.ruaud@smile.fr>
2016-06-20 13:40:48 +0200

    Adding attribute based condition into seller collection to conditionnally build collections of entities by attribute set.

--- Model/ResourceModel/Seller/Collection.php
+++ Model/ResourceModel/Seller/Collection.php
@@ -12,6 +12,18 @@
  */
 namespace Smile\Seller\Model\ResourceModel\Seller;
 
+use Magento\Eav\Model\Config;
+use Magento\Eav\Model\Entity\Collection\AbstractCollection;
+use Magento\Eav\Model\EntityFactory as EavEntityFactory;
+use Magento\Eav\Model\ResourceModel\Helper;
+use Magento\Framework\App\ResourceConnection;
+use Magento\Framework\Data\Collection\Db\FetchStrategyInterface;
+use Magento\Framework\Data\Collection\EntityFactory;
+use Magento\Framework\DB\Adapter\AdapterInterface;
+use Magento\Framework\Event\ManagerInterface;
+use Magento\Framework\Validator\UniversalFactory;
+use Psr\Log\LoggerInterface;
+
 /**
  * Sellers Collection
  *
@@ -19,7 +31,7 @@ namespace Smile\Seller\Model\ResourceModel\Seller;
  * @package  Smile\Seller
  * @author   Romain Ruaud <romain.ruaud@smile.fr>
  */
-class Collection extends \Magento\Eav\Model\Entity\Collection\AbstractCollection
+class Collection extends AbstractCollection
 {
     /**
      * Event prefix
@@ -36,6 +48,60 @@ class Collection extends \Magento\Eav\Model\Entity\Collection\AbstractCollection
     protected $_eventObject = 'smile_seller_entity_collection';
 
     /**
+     * @var null Attribute set id of the entity
+     */
+    private $sellerAttributeSetId = null;
+
+    /**
+     * @var null Attribute set name of the entity
+     */
+    private $sellerAttributeSetName = null;
+
+    /**
+     * Collection constructor.
+     *
+     * @param \Magento\Framework\Data\Collection\EntityFactory             $entityFactory    Entity Factory
+     * @param \Psr\Log\LoggerInterface                                     $logger           Logger
+     * @param \Magento\Framework\Data\Collection\Db\FetchStrategyInterface $fetchStrategy    Fetch Strategy
+     * @param \Magento\Framework\Event\ManagerInterface                    $eventManager     Event Manager
+     * @param \Magento\Eav\Model\Config                                    $eavConfig        EAV Config
+     * @param \Magento\Framework\App\ResourceConnection                    $resource         Resource Connection
+     * @param \Magento\Eav\Model\EntityFactory                             $eavEntityFactory EAV Entity Factory
+     * @param \Magento\Eav\Model\ResourceModel\Helper                      $resourceHelper   Resource Helper
+     * @param \Magento\Framework\Validator\UniversalFactory                $universalFactory Universal Factory
+     * @param \Magento\Framework\DB\Adapter\AdapterInterface|null          $connection       Database Connection
+     * @param null                                                         $attributeSetName Seller Attribute Set Name
+     */
+    public function __construct(
+        EntityFactory $entityFactory,
+        LoggerInterface $logger,
+        FetchStrategyInterface $fetchStrategy,
+        ManagerInterface $eventManager,
+        Config $eavConfig,
+        ResourceConnection $resource,
+        EavEntityFactory $eavEntityFactory,
+        Helper $resourceHelper,
+        UniversalFactory $universalFactory,
+        AdapterInterface $connection = null,
+        $attributeSetName = null
+    ) {
+        $this->sellerAttributeSetName = $attributeSetName;
+
+        parent::__construct(
+            $entityFactory,
+            $logger,
+            $fetchStrategy,
+            $eventManager,
+            $eavConfig,
+            $resource,
+            $eavEntityFactory,
+            $resourceHelper,
+            $universalFactory,
+            $connection
+        );
+    }
+
+    /**
      * Init collection and determine table names
      *
      * @return void
@@ -45,4 +111,45 @@ class Collection extends \Magento\Eav\Model\Entity\Collection\AbstractCollection
         $this->_init('Smile\Seller\Model\Seller', 'Smile\Seller\Model\ResourceModel\Seller');
     }
 
+    /**
+     * Init select. Retrieve only sellers of current attribute set if specified.
+     *
+     * @return $this
+     */
+    protected function _initSelect()
+    {
+        parent::_initSelect();
+
+        if ($this->sellerAttributeSetId == null) {
+            if ($this->sellerAttributeSetName !== null) {
+                $this->sellerAttributeSetId = $this->getAttributeSetIdByName($this->sellerAttributeSetName);
+            }
+        }
+
+        if ($this->sellerAttributeSetId !== null) {
+            $this->addFieldToFilter('attribute_set_id', (int) $this->sellerAttributeSetId);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Retrieve Attribute set data by id or name
+     *
+     * @param int|string|null $attributeSetId The attribute Set Id or Name
+     *
+     * @return mixed
+     */
+    public function getAttributeSetIdByName($attributeSetId)
+    {
+        $select = $this->_resource->getConnection()->select();
+        $field  = 'attribute_set_name';
+        $table  = $this->_resource->getTableName("eav_attribute_set");
+
+        $select->from($table, "attribute_set_id")
+            ->where($this->_getConditionSql("entity_type_id", ['eq' => $this->getEntity()->getTypeId()]))
+            ->where($this->_getConditionSql($field, ['eq' => $attributeSetId]));
+
+        return $this->_resource->getConnection()->fetchOne($select);
+    }
 }
