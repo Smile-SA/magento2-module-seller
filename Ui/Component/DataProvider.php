3eb7bb5d2923b96ab832ff29380e5dabac874393
Romain Ruaud <romain.ruaud@smile.fr>
2016-06-20 13:39:45 +0200

    Moving generic data providers for UI components into Seller module.

---
+++ Ui/Component/DataProvider.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ * DISCLAIMER
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future.
+ *
+ * @category  Smile
+ * @package   Smile\Seller
+ * @author    Romain Ruaud <romain.ruaud@smile.fr>
+ * @copyright 2016 Smile
+ * @license   Open Software License ("OSL") v. 3.0
+ */
+namespace Smile\Seller\Ui\Component;
+
+/**
+ * Data Provider for UI components based on Sellers
+ *
+ * @category Smile
+ * @package  Smile\Seller
+ * @author   Romain Ruaud <romain.ruaud@smile.fr>
+ */
+class DataProvider extends \Magento\Ui\DataProvider\AbstractDataProvider
+{
+    /**
+     * Seller collection
+     *
+     * @var \Smile\Seller\Model\ResourceModel\Seller\Collection
+     */
+    protected $collection;
+
+    /**
+     * @var \Magento\Ui\DataProvider\AddFieldToCollectionInterface[]
+     */
+    protected $addFieldStrategies;
+
+    /**
+     * @var \Magento\Ui\DataProvider\AddFilterToCollectionInterface[]
+     */
+    protected $addFilterStrategies;
+
+    /**
+     * Construct
+     *
+     * @param string                                                    $name                 Component name
+     * @param string                                                    $primaryFieldName     Primary field Name
+     * @param string                                                    $requestFieldName     Request field name
+     * @param CollectionFactory                                         $collectionFactory    The collection factory
+     * @param \Magento\Ui\DataProvider\AddFieldToCollectionInterface[]  $addFieldStrategies   Add field Strategy
+     * @param \Magento\Ui\DataProvider\AddFilterToCollectionInterface[] $addFilterStrategies  Add filter Strategy
+     * @param array                                                     $meta                 Component Meta
+     * @param array                                                     $data                 Component extra data
+     */
+    public function __construct(
+        $name,
+        $primaryFieldName,
+        $requestFieldName,
+        $collectionFactory,
+        array $addFieldStrategies = [],
+        array $addFilterStrategies = [],
+        array $meta = [],
+        array $data = []
+    ) {
+        parent::__construct($name, $primaryFieldName, $requestFieldName, $meta, $data);
+
+        $this->collection = $collectionFactory->create();
+        $this->collection->addAttributeToSelect("name");
+
+        $this->addFieldStrategies  = $addFieldStrategies;
+        $this->addFilterStrategies = $addFilterStrategies;
+    }
+
+    /**
+     * Get data
+     *
+     * @return array
+     */
+    public function getData()
+    {
+        if (!$this->getCollection()->isLoaded()) {
+            $this->getCollection()->load();
+        }
+        $items = $this->getCollection()->toArray();
+
+        return [
+            'totalRecords' => $this->getCollection()->getSize(),
+            'items'        => array_values($items),
+        ];
+    }
+
+    /**
+     * Add field to select
+     *
+     * @param string|array $field
+     * @param string|null  $alias
+     *
+     * @return void
+     */
+    public function addField($field, $alias = null)
+    {
+        if (isset($this->addFieldStrategies[$field])) {
+            $this->addFieldStrategies[$field]->addField($this->getCollection(), $field, $alias);
+        } else {
+            parent::addField($field, $alias);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function addFilter(\Magento\Framework\Api\Filter $filter)
+    {
+        if (isset($this->addFilterStrategies[$filter->getField()])) {
+            $this->addFilterStrategies[$filter->getField()]
+                ->addFilter(
+                    $this->getCollection(),
+                    $filter->getField(),
+                    [$filter->getConditionType() => $filter->getValue()]
+                );
+        } else {
+            parent::addFilter($filter);
+        }
+    }
+}
---
+++ Ui/Component/Listing/Column/Seller/Options.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * DISCLAIMER
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future.
+ *
+ * @category  Smile
+ * @package   Smile\Seller
+ * @author    Romain Ruaud <romain.ruaud@smile.fr>
+ * @copyright 2016 Smile
+ * @license   Open Software License ("OSL") v. 3.0
+ */
+namespace Smile\Seller\Ui\Component\Listing\Column\Seller;
+
+use Magento\Store\Ui\Component\Listing\Column\Store\Options as StoreOptions;
+
+/**
+ * Options column for Sellers based UI components.
+ *
+ * @category Smile
+ * @package  Smile\Seller
+ * @author   Romain Ruaud <romain.ruaud@smile.fr>
+ */
+class Options extends StoreOptions
+{
+    /**
+     * All Store Views value
+     */
+    const ALL_STORE_VIEWS = '0';
+
+    /**
+     * Get options
+     *
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        if ($this->options !== null) {
+            return $this->options;
+        }
+
+        $this->currentOptions['All Store Views']['label'] = __('All Store Views');
+        $this->currentOptions['All Store Views']['value'] = self::ALL_STORE_VIEWS;
+
+        $this->generateCurrentOptions();
+
+        $this->options = array_values($this->currentOptions);
+
+        return $this->options;
+    }
+}
